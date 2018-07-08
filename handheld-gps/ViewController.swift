b748981d5d95ef4d0fafb58768fe94598b7a3740
dgriffiths3 <dgriffiths3@mac.com>
2018-07-08 16:32:02 +0100

    Initial Commit

---
+++ handheld-gps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  handheld-gps
+//
+//  Created by David Griffiths on 08/07/2018.
+//  Copyright © 2018 David Griffiths. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
